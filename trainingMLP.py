3c3d3e1cdd5b933db2765a854be9e5c41118c182
Pedro Alves <pepedro2512@gmail.com>
2022-10-12 19:40:02 -0300

    UPDATE function trainingMLP

--- trainingMLP.py
+++ trainingMLP.py
@@ -2,14 +2,21 @@ import tensorflow as tf
 
 
 def training_MLP(X, y, Xt, nsteps, noutputs):
-    # Defining the model
-    model = tf.keras.Sequential()
-    model.add(tf.keras.layers.Dense(20, activation='relu', input_dim=nsteps))
-    model.add(tf.keras.layers.Dense(20, activation='relu'))
-    model.add(tf.keras.layers.Dense(noutputs))
-    model.compile(optimizer='adam', loss='mse')
+    try:
+        X.shape
+        y.shape
+        Xt.shape
+        # Defining the model
+        model = tf.keras.Sequential()
+        model.add(tf.keras.layers.Dense(
+            20, activation='relu', input_dim=nsteps))
+        model.add(tf.keras.layers.Dense(20, activation='relu'))
+        model.add(tf.keras.layers.Dense(noutputs))
+        model.compile(optimizer='adam', loss='mse')
 
-    # Fitting the model
-    model.fit(X, y, epochs=100, verbose=0)
-    out = model.predict(Xt, verbose=0)
-    return out, model
+        # Fitting the model
+        model.fit(X, y, epochs=100, verbose=0)
+        out = model.predict(Xt, verbose=0)
+        return out, model
+    except:
+        print('Data passed do not required criteria')
