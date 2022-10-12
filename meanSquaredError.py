a37952e7b70ff30d3ff78f4dd3735f3afad4cd84
Pedro Alves <pepedro2512@gmail.com>
2022-10-12 16:06:11 -0300

    Funtion that will calculate the Mean Squared Error between previous and forecast data

---
+++ meanSquaredError.py
@@ -0,0 +1,18 @@
+def MeanSquaredError(expected, forecasted):
+    '''
+    This function will take two arrays with the same dimension and calculate
+    the mean squared error between the array forecasted and the expected.
+
+    Example:
+        >> import numpy as np
+        >> a = np.array([1,2,3])
+        >> b = np.array([4,5,6])
+        >> print(MeanSquaredError(a,b))
+        >> 243
+
+    '''
+    if expected.shape != forecasted.shape:
+        print(
+            f"Shapes do not match expected{expected.shape} x forecasted {forecasted.shape}")
+    else:
+        return (pow((expected-forecasted), 2).sum())/len(forecasted)
