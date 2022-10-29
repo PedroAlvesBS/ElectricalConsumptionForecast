71084597c3d9d24e2cbd77a2b9ec1297f0637cd3
Pedro Alves <pepedro2512@gmail.com>
2022-10-29 15:35:57 -0300

    UPDATE added neuron layer control

--- trainingMLP.py
+++ trainingMLP.py
@@ -1,22 +1,35 @@
 import tensorflow as tf
 
 
-def training_MLP(X, y, Xt, nsteps, noutputs):
+def training_MLP(X, y, Xt, nsteps, L1, L2, noutputs):
     try:
         X.shape
         y.shape
         Xt.shape
-        # Defining the model
-        model = tf.keras.Sequential()
-        model.add(tf.keras.layers.Dense(
-            20, activation='relu', input_dim=nsteps))
-        model.add(tf.keras.layers.Dense(20, activation='relu'))
-        model.add(tf.keras.layers.Dense(noutputs))
-        model.compile(optimizer='adam', loss='mse')
+        if L1 == 0 or L2 == 0:
+            # Defining the model
+            model = tf.keras.Sequential()
+            model.add(tf.keras.layers.Dense(
+                L1+L2, activation='relu', input_dim=nsteps))
+            model.add(tf.keras.layers.Dense(noutputs))
+            model.compile(optimizer='adam', loss='mse')
 
-        # Fitting the model
-        model.fit(X, y, epochs=100, verbose=0)
-        out = model.predict(Xt, verbose=0)
-        return out, model
+            # Fitting the model
+            model.fit(X, y, epochs=100, verbose=0)
+            out = model.predict(Xt, verbose=0)
+            return out, model
+        else:
+            # Defining the model
+            model = tf.keras.Sequential()
+            model.add(tf.keras.layers.Dense(
+                L1, activation='relu', input_dim=nsteps))
+            model.add(tf.keras.layers.Dense(L2, activation='relu'))
+            model.add(tf.keras.layers.Dense(noutputs))
+            model.compile(optimizer='adam', loss='mse')
+
+            # Fitting the model
+            model.fit(X, y, epochs=100, verbose=0)
+            out = model.predict(Xt, verbose=0)
+            return out, model
     except:
         print('Data passed do not required criteria')
